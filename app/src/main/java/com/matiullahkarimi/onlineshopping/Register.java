e451800ed308313ed5d261e9441ea24cb89970f6
Matiullah <matiullah.karimi@gmail.com>
2016-10-19 09:54:26 +0430

    register functionality added

--- app/src/main/java/com/matiullahkarimi/onlineshopping/Register.java
+++ app/src/main/java/com/matiullahkarimi/onlineshopping/Register.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 
@@ -27,6 +28,10 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
 
         helper = new Helper();
 
@@ -79,6 +84,17 @@ public class Register extends AppCompatActivity {
                 helper.toast(Register.this, "Something went wrong");
             }
         });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
